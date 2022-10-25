5d9993868b978110be6009434e3f16dba61bd304
qajagbe <quadri.ajagbe@infocorp.io>
2022-10-25 07:33:41 +0100

    Add project files.

---
+++ AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CashEX.API
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+    }
+}
+
